6fa58e08e3d9b13457505f00bd6c30b7488672f7
Lindsey Pollan <lpollan@live.com>
2022-06-20 14:10:08 -0500

    complete

---
+++ app.js
@@ -0,0 +1,4 @@
+//make the html elements js elements (getElementById and grab the id of the form)
+//if that doesn't work create each element individually and put them in an array which with be called in the following statement
+//if statement to make the register button output user information at bottom of page except password
+//if... then document.write(<whatever i called the stuff in the form>)
